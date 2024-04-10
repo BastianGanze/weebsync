7af5502e860f136acf6c49acf7c146651f462568
Bastian Ganze <bastianganze@gmail.com>
2024-04-10 16:31:06 +0200

    fix shared alias missing in vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,31 +2,29 @@ import { join } from "path";
 import Vue from "@vitejs/plugin-vue";
 import UnpluginVueComponents from "unplugin-vue-components";
 import VitePluginVuetify from "vite-plugin-vuetify";
-import {defineConfig} from "vite";
+import { defineConfig } from "vite";
 
 const resolve = (dir: string) => join(__dirname, dir);
 
-export default defineConfig(
-    ({command}) => {
-
-      return {
-        resolve: {
-          alias: {
-            "@": resolve("src"),
-          },
-        },
-        base: "",
-        define: {
-          __HOST__:  command === 'serve' ? '"http://0.0.0.0:42380"' : '""',
-        },
-        build: {
-          outDir: "../build/client",
-          target: "es2015",
-        },
-        plugins: [Vue(), UnpluginVueComponents.vite(), VitePluginVuetify()],
-        server: {
-          port: 8080,
-        },
-      };
-    }
-);
+export default defineConfig(({ command }) => {
+  return {
+    resolve: {
+      alias: {
+        "@": resolve("src"),
+        "@shared": resolve("../shared"),
+      },
+    },
+    base: "",
+    define: {
+      __HOST__: command === "serve" ? '"http://0.0.0.0:42380"' : '""',
+    },
+    build: {
+      outDir: "../build/client",
+      target: "es2015",
+    },
+    plugins: [Vue(), UnpluginVueComponents.vite(), VitePluginVuetify()],
+    server: {
+      port: 8080,
+    },
+  };
+});
